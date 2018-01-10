dd84fa20ac135907a3161076b113919014ca5511
Eugen Wintersberger <eugen.wintersberger@desy.de>
2018-01-10 17:16:39 +0100

    Updated the test conan_file.py to version 1.0.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,7 +8,7 @@ class H5cppTestConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure(source_dir=self.source_folder, build_dir=self.build_folder)
         cmake.build()
 
     def imports(self):
